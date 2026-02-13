7495859f086b74ac6b7ff9d8d8c0b6f937182073
Js@28 <shahjainish38837@gmail.com>
2026-02-13 21:11:44 +1100

    redo main

--- weather-frontend/src/main.jsx
+++ weather-frontend/src/main.jsx
@@ -14,11 +14,13 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import { store } from "./app/store";
+import { store }  from "./app/store";
+import { fetchMe } from "./features/auth/authThunks";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
-
+store.dispatch(fetchMe()).finally(() => {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+});
